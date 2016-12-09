c9aac3ad2f125a0226675802403e3af020c511c4
Ian Solliec <ian@contiamo.com>
2016-12-09 11:35:22 +0100

    Test API errors with VCR.

--- contiamo/tests/test_errors.py
+++ contiamo/tests/test_errors.py
@@ -1,4 +1,5 @@
 import unittest
+import vcr
 
 from contiamo.resources import *
 from contiamo.errors import *
@@ -6,16 +7,19 @@ from contiamo.errors import *
 
 class ErrorTestCase(unittest.TestCase):
 
-  def _make_erroneous_request(self, api_base, project_id, dashboard_id):
-    contiamo_client = Client('token', api_base=api_base)
+  def _make_erroneous_request(self, token, api_base, project_id, dashboard_id):
+    contiamo_client = Client(token, api_base=api_base)
     project = contiamo_client.Project(project_id)
     dashboard = project.Dashboard.retrieve(dashboard_id)
 
   def test_connection_error(self):
     with self.assertRaises(APIConnectionError):
-      self._make_erroneous_request('http://xyz.wrong-domain-name.123', '456', '789')
+      self._make_erroneous_request('token', 'http://xyz.wrong-domain-name.123', '456', '789')
 
-  # TODO: make cassette for other errors
+  @vcr.use_cassette('tests/cassettes/test_auth_error.yaml')
+  def test_auth_error(self):
+    with self.assertRaises(AuthenticationError):
+      self._make_erroneous_request('wrongaccesstoken', 'https://api.contiamo.com', '48590121', '345')
 
 
 if __name__ == '__main__':
--- contiamo/utils.py
+++ contiamo/utils.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 
 ###
 # parse query id
