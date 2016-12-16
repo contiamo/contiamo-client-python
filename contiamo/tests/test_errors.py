ff4bb2749980614b11bcd7ac92f0809b713ebb99
Ian Solliec <ian@contiamo.com>
2016-12-16 10:08:32 +0100

    Add new, and simplify error tests.

--- contiamo/tests/test_errors.py
+++ contiamo/tests/test_errors.py
@@ -1,21 +1,22 @@
 import unittest
 import responses
-import vcr
 
 from contiamo.resources import *
 from contiamo.public import query
+from contiamo.data import DataClient
+from contiamo.utils import contract_url_template_from_identifier
 from contiamo.errors import *
 
 
-def instantiate_resource(api_key='apikey', api_base='https://api.base', project_id='123', resource_id='456'):
-  contiamo_client = Client(api_key, api_base=api_base)
-  project = contiamo_client.Project(project_id)
-  resource = project.Dashboard(resource_id)
-  return (resource, resource_id)
+# the only meaningful parameter is api_base, which should not resolve
+api_base = 'http://xyz.wrong-domain-name.123'
+resource_id = '456'
+contiamo_client = Client('some_api_key', api_base=api_base)
+project = contiamo_client.Project(project_id='123')
+resource = project.Dashboard(resource_id)
 
-def make_erroneous_request(method, response_body, status, payload=None, **kwargs):
-  resource, resource_id = instantiate_resource(**kwargs)
-
+# resources module
+def mock_erroneous_request(method, response_body, status, payload=None):
   url = resource.class_url() if method == 'create' else resource.instance_url()
   verb = {'retrieve': responses.GET, 'create': responses.POST, 'modify': responses.PUT}.get(method)
   responses.add(verb, url, body=response_body, status=status, content_type='application/json')
@@ -24,76 +25,86 @@ def make_erroneous_request(method, response_body, status, payload=None, **kwargs
   request_method = getattr(resource, method)
   request_method(argument)
 
-def make_erroneous_retrieve(response_body, status, **kwargs):
-  make_erroneous_request('retrieve', response_body, status, **kwargs)
+def mock_erroneous_retrieve(response_body, status):
+  mock_erroneous_request('retrieve', response_body, status)
 
-def make_erroneous_create(response_body, status, **kwargs):
-  make_erroneous_request('create', response_body, status, payload={'name': 'ResourceName'}, **kwargs)
+def mock_erroneous_create(response_body, status):
+  mock_erroneous_request('create', response_body, status, payload={'name': 'ResourceName'})
 
-def make_erroneous_update(response_body, status, **kwargs):
-  make_erroneous_request('modify', response_body, status, payload={'name': 'AnotherName'}, **kwargs)
+def mock_erroneous_update(response_body, status):
+  mock_erroneous_request('modify', response_body, status, payload={'name': 'AnotherName'})
 
-def make_erroneous_query(response_body, status):
+# public module
+def mock_erroneous_query(response_body, status):
   query_id = 'query:olap:48590200:21237:randomquerytoken'
-  query_url = 'https://api.contiamo.com/48590200/stored_query/21237.json'
+  query_url = api_base + '/48590200/stored_query/21237.json'  # strip token parameter
   responses.add(responses.GET, query_url, body=response_body, status=status, content_type='application/json')
-  query(query_id)
+  query(query_id, api_base=api_base)
+
+# data module
+def mock_erroneous_upload(response_body, status):
+  contract_id = 'contract:48590121:666570779:test'
+  contract_url = contract_url_template_from_identifier(contract_id, api_base).format(action='upload/process')
+  responses.add(responses.POST, contract_url, body=response_body, status=status, content_type='application/json')
+  data_client = DataClient(contract_id, 'contract_token', api_base=api_base)
+  data_client.upload(filename='tests/data/mock_data.csv')
 
 
 class ErrorTestCase(unittest.TestCase):
 
   def test_connection_error(self):
-    resource, resource_id = instantiate_resource(api_base='http://xyz.wrong-domain-name.123')
     with self.assertRaises(APIConnectionError):
       resource.retrieve(resource_id)
 
-  @vcr.use_cassette('tests/cassettes/test_auth_error.yaml')
+  @responses.activate
   def test_auth_error(self):
-    resource, resource_id = instantiate_resource(api_key='wrong_api_key', api_base='https://api.contiamo.com')
     with self.assertRaises(AuthenticationError):
-      resource.retrieve(resource_id)
+      mock_erroneous_retrieve('{"error":"not_found","logged_in":false}', 404)
+    with self.assertRaises(AuthenticationError):
+      mock_erroneous_retrieve('{"error":"server_error","logged_in":false}', 500)
 
   @responses.activate
   def test_notfound_error(self):
     with self.assertRaises(NotFoundError):
-      make_erroneous_retrieve('{"error":"not_found","logged_in":true}', 404)
+      mock_erroneous_retrieve('{"error":"not_found","logged_in":true}', 404)
 
   @responses.activate
   def test_api_error(self):
     with self.assertRaises(APIError):
-      make_erroneous_retrieve('{}', 500)
+      mock_erroneous_retrieve('{}', 500)
 
   @responses.activate
   def test_create_error(self):
     with self.assertRaises(UpdateError):
-      make_erroneous_create('{}', 422)
+      mock_erroneous_create('{}', 422)
 
   @responses.activate
   def test_update_error(self):
     with self.assertRaises(UpdateError):
-      make_erroneous_update('{}', 409)
+      mock_erroneous_update('{}', 409)
     with self.assertRaises(UpdateError):
-      make_erroneous_update('{}', 422)
+      mock_erroneous_update('{}', 422)
 
   @responses.activate
   def test_invalid_response(self):
     with self.assertRaises(ResponseError):
-      make_erroneous_retrieve('{"invalid":"json"', 200)
+      mock_erroneous_retrieve('{"invalid":"json"', 200)
     with self.assertRaises(ResponseError):
-      make_erroneous_query('{"invalid":"json"', 200)
+      mock_erroneous_query('{"invalid":"json"', 200)
 
   @responses.activate
   def test_data_source_error(self):
     with self.assertRaises(DataSourceError):
-      make_erroneous_query('{}', 412)
-    # TODO: add data client error
+      mock_erroneous_query('{}', 412)
+    with self.assertRaises(DataSourceError):
+      mock_erroneous_upload('{}', 412)
 
   @responses.activate
   def test_query_error(self):
     with self.assertRaises(QueryError):
-      make_erroneous_query('{}', 410)
+      mock_erroneous_query('{}', 410)
     with self.assertRaises(QueryError):
-      make_erroneous_query('{}', 424)
+      mock_erroneous_query('{}', 424)
 
 
 if __name__ == '__main__':
